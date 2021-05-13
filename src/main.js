b80823cd0420dc55959839a071e58adfc1e5d742
DevKeone <keone.somers@outlook.com>
2021-05-13 18:03:25 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDt0YLf5BZUFyIwFhdFE5n9rBi14iBgSHs",
+  authDomain: "codecave-6f040.firebaseapp.com",
+  projectId: "codecave-6f040",
+  storageBucket: "codecave-6f040.appspot.com",
+  messagingSenderId: "263589621919",
+  appId: "1:263589621919:web:4f0ada545240ab7fb8f2aa",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
